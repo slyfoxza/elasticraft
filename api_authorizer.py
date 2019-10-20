834c5578b6a9221acaa8ea62f620751f0d681c60
Philip Cronje <syke107@gmail.com>
2019-10-20 18:14:36 +0200

    Replace IAM auth with mock custom authorizer

--- api_authorizer.py
+++ api_authorizer.py
@@ -8,21 +8,5 @@
 # Unless required by applicable law or agreed to in writing, software distributed under the License is
 # distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and limitations under the License.
-.PHONY: all clean
-
-all: api_authorizer.py.zip server_post.zip snapshot_on_shutdown.zip
-
-clean:
-	$(RM) api_authorizer.py.zip server_post.zip snapshot_on_shutdown.zip
-
-pip-install.timestamp: requirements.txt
-	$(RM) -r lambda-dependencies && mkdir -p lambda-dependencies
-	pip3 install -r $< -t lambda-dependencies --no-compile --no-deps && touch $@
-
-%.zip: %.py pip-install.timestamp
-	$(RM) $@ && zip -9 -ll $@ $< && \
-		cd lambda-dependencies && \
-		zip -9 -r ../$@ . -x bin/ bin/* *.dist-info/ *.dist-info/*
-
-%.py.zip: %.py
-	$(RM) $@ && zip -9 -ll $@ $<
+def handler(event, context):
+    raise Exception("Unauthorized")
